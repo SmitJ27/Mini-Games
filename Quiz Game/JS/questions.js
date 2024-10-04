2919c40322a6541cde6d9eda23a531266ccf139d
Smit Joshi <104095303+SmitJ27@users.noreply.github.com>
2024-10-04 13:10:09 +0530

    Add files via upload

---
+++ Quiz Game/JS/questions.js
@@ -0,0 +1,114 @@
+// Creating an array and passing the number, questions, options, and answers
+let questions = [
+  {
+    numb: 1,
+    question: "Which planet is covered by clouds of sulphuric acid?",
+    answer: "b) Venus",
+    options: [
+      "a) Earth",
+      "b) Venus",
+      "c) Mars",
+      "d) Jupiter"
+    ]
+  },
+  {
+    numb: 2,
+    question: "What is the largest planet in our solar system?",
+    answer: "d) Jupiter",
+    options: [
+      "a) Earth",
+      "b) Venus",
+      "c) Saturn",
+      "d) Jupiter"
+    ]
+  },
+  {
+    numb: 3,
+    question: "Which planet is known as the 'Red Planet'?",
+    answer: "c) Mars",
+    options: [
+      "a) Mercury",
+      "b) Venus",
+      "c) Mars",
+      "d) Jupiter"
+    ]
+  },
+  {
+    numb: 4,
+    question: "What is the name of the spacecraft that landed the first humans on the Moon in 1969?",
+    answer: "a) Apollo 11",
+    options: [
+      "a) Apollo 11",
+      "b) Voyager 1",
+      "c) Hubble Space Telescope",
+      "d) Curiosity"
+    ]
+  },
+  {
+    numb: 5,
+    question: "Which famous space telescope was launched into orbit in 1990 and has provided stunning images of distant galaxies and nebulae?",
+    answer: "c) Hubble Space Telescope",
+    options: [
+      "a) Kepler Space Telescope",
+      "b) James Webb Space Telescope",
+      "c) Hubble Space Telescope",
+      "d) Chandra X-ray Observatory"
+    ]
+  },
+  {
+    numb: 6,
+    question: "What is the term for a small, rocky object that orbits the Sun and is often referred to as a 'minor planet'?",
+    answer: "c) Asteroid",
+    options: [
+      "a) Meteoroid",
+      "b) Comet",
+      "c) Asteroid",
+      "d) Moon"
+    ]
+  },
+
+  {
+    numb: 7,
+    question: "What is the name of the theoretical boundary beyond which nothing, not even light, can escape the gravitational pull of a black hole?",
+    answer: "b) Event Horizon",
+    options: [
+      "a) Singularity",
+      "b) Event Horizon",
+      "c) Wormhole",
+      "d) Event Center"
+    ]
+  },
+  {
+    numb: 8,
+    question: "Which spacecraft was the first to successfully land on Saturn's moon Titan in 2005, sending back valuable data and images?",
+    answer: "a) Huygens Probe (part of the Cassini-Huygens mission)",
+    options: [
+      "a) Huygens Probe (part of the Cassini-Huygens mission)",
+      "b) Voyager 1",
+      "c) Curiosity Rover",
+      "d) Mars Reconnaissance Orbiter"
+    ]
+  },
+  {
+    numb: 9,
+    question: "What is the most common element in the universe by mass?",
+    answer: "a) Hydrogen",
+    options: [
+      "a) Hydrogen",
+      "b) Helium",
+      "c) Oxygen",
+      "d) Carbon"
+    ]
+  },
+  {
+    numb: 10,
+    question: "What is the name of the concept that suggests the universe is constantly expanding, originally discovered through the redshift of distant galaxies?",
+    answer: "d) The Big Bang Theory",
+    options: [
+      "a) The Steady State Theory",
+      "b) The Pulsar Theory",
+      "c) The Nebular Hypothesis",
+      "d) The Big Bang Theory"
+    ]
+  },
+];
